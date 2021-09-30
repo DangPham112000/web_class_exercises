dea2f1d5b1a07548a9fa38881a12a51c758573d8
DangPham <dangpham112000@users.noreply.github.com>
2021-09-30 23:15:47 +0700

    basic rule chess move

--- chess/script.js
+++ chess/script.js
@@ -60,6 +60,15 @@ function setupDragDrop(obj) {
             this.oldY = e.clientY;
         }
     }; 
+
+    obj.onmouseleave = function(e) {
+        if (this.isDown) {
+            this.isDown = false;
+            this.style['z-index'] = 1;
+            obj.style.left = "50%";
+            obj.style.top = "50%";
+        }
+    }
 }
 
 function move(obj) {
@@ -71,95 +80,150 @@ function move(obj) {
     // console.log('MOVEEEEEEEE')
     console.log(stepX, stepY);
     console.log(obj.classList[1]);
-    if (!canIMove(obj, stepX, stepY))
-    {
-        stepX = 0;
-        stepY = 0;
-    }
     for (let i=0; i<8; i++) {
         for (let j=0; j<8; j++) {
             if (boxes[i][j] === obj.offsetParent) {
                 // console.log(i,j);
                 // console.log(boxes[i][j]);
                 if (boxes[i - stepY][j + stepX]) {
+                    if (!canIMove(obj, stepX, stepY, i, j))
+                    {
+                        stepX = 0;
+                        stepY = 0;
+                    }
                     // console.log(obj);
                     // console.log(boxes[i - stepY][j + stepX]);
-                    
                     boxes[i - stepY][j + stepX].appendChild(obj);
                     obj.style.left = "50%";
                     obj.style.top = "50%";
                     return;
                 }
+                boxes[i][j].appendChild(obj);
+                obj.style.left = "50%";
+                obj.style.top = "50%";
+                return;
             }
         }
     }    
 }
 
-function canIMove(obj, stepX, stepY) {
+function canIMove(obj, stepX, stepY, i, j) {
     const func = obj.classList[1];
+    // they can't eat if same faction
+    if ((boxes[i - stepY][j + stepX].children.length === 2) &&
+        (boxes[i - stepY][j + stepX].children[1].classList[2] === obj.classList[2])) 
+    {
+        return false;
+    }
     switch (func) {
         case 'pawn':
             // care about they faction
             const faction = obj.classList[2];
-            if (stepX === -1 || stepX === 1) {
-                return canIEat();
+            if (Math.abs(stepX) === Math.abs(stepY)) {
+                if (faction === 'white' && stepY === -1) {
+                    return canIEat(obj, stepX, stepY, i, j);
+                } else if (faction === 'black' && stepY === 1) {
+                    return canIEat(obj, stepX, stepY, i, j);
+                } else {
+                    return false;
+                }
             }
             if (stepX != 0) {
                 return false;
             }
             if (faction === 'white') {
-                if (stepY === -1 || (stepY === -2 && obj.firstStep === true)) {
-                    obj.firstStep = false;
-                        if (!IsStuck()) {
+                if (stepY === -1) {
+                    if (!doIStuck(obj, stepX, stepY, i, j)) {
+                        obj.firstStep = false;
                         return true;
+                    } else {
+                        return false;
                     }
                 }
-            } else if (faction === 'black') {
-                if (stepY === 1 || (stepY === 2 && obj.firstStep === true)) { 
-                    obj.firstStep = false;
-                        if (!IsStuck()) {
+                if (stepY === -2 && obj.firstStep === true) {
+                    if (!doIStuck(obj, stepX, stepY, i, j)) {
+                        obj.firstStep = false;
                         return true;
+                    } else {
+                        return false;
                     }
                 }
+                return false;
+            } else if (faction === 'black') {                
+                if (stepY === 1) {
+                    if (!doIStuck(obj, stepX, stepY, i, j)) {
+                        obj.firstStep = false;
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+                if (stepY === 2 && obj.firstStep === true) {
+                    if (!doIStuck(obj, stepX, stepY, i, j)) {
+                        obj.firstStep = false;
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
             }
             return false;
         
         case 'rook':
             if (stepX === 0 || stepY === 0) {
-                if (!IsStuck()) {
+                if (!doIStuck(obj, stepX, stepY, i, j)) {
+                    if (boxes[i - stepY][j + stepX].children.length === 2) {
+                        return canIEat(obj, stepX, stepY, i, j);
+                    }
                     return true;
+                } else {
+                    return false;
                 }
             }
             return false;
         
         case 'bishop':
             if (Math.abs(stepX) === Math.abs(stepY)) {
-                if (!IsStuck()) {
+                if (!doIStuck(obj, stepX, stepY, i, j)) {
+                    if (boxes[i - stepY][j + stepX].children.length === 2) {
+                        return canIEat(obj, stepX, stepY, i, j);
+                    }
                     return true;
+                } else {
+                    return false;
                 }
             }
             return false;
         
         case 'queen':
             if ((Math.abs(stepX) === Math.abs(stepY)) || ((stepX === 0 || stepY === 0))) {
-                if (!IsStuck()) {
+                if (!doIStuck(obj, stepX, stepY, i, j)) {
+                    if (boxes[i - stepY][j + stepX].children.length === 2) {
+                        return canIEat(obj, stepX, stepY, i, j);
+                    }
                     return true;
+                } else {
+                    return false;
                 }
             }
             return false;
         
         case 'king':
             if (Math.abs(stepX) <= 1 && Math.abs(stepY) <= 1) {
-                if (!IsStuck()) {
-                    return true;
+                if (boxes[i - stepY][j + stepX].children.length === 2) {
+                    return canIEat(obj, stepX, stepY, i, j);
                 }
+                return true;
             }
             return false;
         
         case 'knight':
-            if ((Math.abs(stepX) === 1 && Math.abs(stepY) === 2) || (Math.abs(stepX) === 2 && Math.abs(stepY) === 1)) {
-                if (!IsStuck()) {
-                    return true;
+            if ((Math.abs(stepX) === 1 && Math.abs(stepY) === 2) || 
+                (Math.abs(stepX) === 2 && Math.abs(stepY) === 1)) 
+            {
+                if (boxes[i - stepY][j + stepX].children.length === 2) {
+                    return canIEat(obj, stepX, stepY, i, j);
                 }
             }
             return false;
@@ -169,10 +233,188 @@ function canIMove(obj, stepX, stepY) {
     }
 }
 
-function canIEat() {
+function canIEat(obj, stepX, stepY, i, j) {
+    if ((boxes[i - stepY][j + stepX].children.length === 2) &&
+        (boxes[i - stepY][j + stepX].children[1].classList[2] !== obj.classList[2])) // different faction
+    {
+        boxes[i - stepY][j + stepX].children[1].remove();
+        return true;
+    }
+    
     return false;
 }
 
-function IsStuck() {    
-    return false;
+function doIStuck(obj, stepX, stepY, i, j) {
+    const func = obj.classList[1];
+    switch (func) {
+        case 'pawn':
+            const faction = obj.classList[2];
+            if (faction === 'white') {
+                if (stepY === -1) {
+                    if (boxes[i - stepY][j].children.length === 2) {
+                        return true;
+                    }
+                }
+                if (stepY === -2) {
+                    if ((boxes[i - stepY][j].children.length === 2) ||
+                        (boxes[i + 1][j].children.length === 2)) 
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            } else if (faction === 'black') {                
+                if (stepY === 1) {
+                    if (boxes[i - stepY][j].children.length === 2) {
+                        return true;
+                    }
+                }
+                if (stepY === 2) {
+                    if ((boxes[i - stepY][j].children.length === 2) ||
+                        (boxes[i - 1][j].children.length === 2)) 
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            return false;
+        
+        case 'rook':
+            if (stepX < 0) {
+                for (let i2=stepX+1; i2<0; i2++) {
+                    if (boxes[i - stepY][j + i2].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            if (stepX > 0) {
+                for (let i2=1; i2<stepX; i2++) {
+                    if (boxes[i - stepY][j + i2].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            if (stepY < 0) {
+                for (let i2=stepY+1; i2<0; i2++) {
+                    if (boxes[i - i2][j + stepX].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            if (stepY > 0) {
+                for (let i2=1; i2<stepY; i2++) {
+                    if (boxes[i - i2][j + stepX].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        
+        case 'bishop':
+            if (stepX === stepY & stepX < 0) {
+                for (let i2=1; i2<-stepX; i2++) {
+                    if (boxes[i + i2][j - i2].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            if (stepX === stepY & stepX > 0) {
+                for (let i2=1; i2<stepX; i2++) {
+                    if (boxes[i - i2][j + i2].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            if (stepX !== stepY & stepX > 0) {
+                for (let i2=1; i2<stepX; i2++) {
+                    if (boxes[i + i2][j + i2].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            if (stepX !== stepY & stepX < 0) {
+                for (let i2=1; i2<-stepX; i2++) {
+                    if (boxes[i - i2][j - i2].children.length === 2) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        
+        case 'queen':
+            if (stepX === 0 || stepY === 0) {
+                if (stepX < 0) {
+                    for (let i2=stepX+1; i2<0; i2++) {
+                        if (boxes[i - stepY][j + i2].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+                if (stepX > 0) {
+                    for (let i2=1; i2<stepX; i2++) {
+                        if (boxes[i - stepY][j + i2].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+                if (stepY < 0) {
+                    for (let i2=stepY+1; i2<0; i2++) {
+                        if (boxes[i - i2][j + stepX].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+                if (stepY > 0) {
+                    for (let i2=1; i2<stepY; i2++) {
+                        if (boxes[i - i2][j + stepX].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            if (Math.abs(stepX) === Math.abs(stepY)) {
+                if (stepX === stepY & stepX < 0) {
+                    for (let i2=1; i2<-stepX; i2++) {
+                        if (boxes[i + i2][j - i2].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+                if (stepX === stepY & stepX > 0) {
+                    for (let i2=1; i2<stepX; i2++) {
+                        if (boxes[i - i2][j + i2].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+                if (stepX !== stepY & stepX > 0) {
+                    for (let i2=1; i2<stepX; i2++) {
+                        if (boxes[i + i2][j + i2].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+                if (stepX !== stepY & stepX < 0) {
+                    for (let i2=1; i2<-stepX; i2++) {
+                        if (boxes[i - i2][j - i2].children.length === 2) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        
+        case 'king':
+            
+            return false;
+        
+        case 'knight':
+            
+            return false;
+        
+        default:
+            return false;
+    }
 }
