e15c81831ebb14d0c306f3f0c1c3ad8453c1a502
DangPham <dangpham112000@users.noreply.github.com>
2021-09-30 10:56:16 +0700

    chess move with own chess rules but ...

--- chess/script.js
+++ chess/script.js
@@ -1,8 +1,4 @@
 
-
-const whitePawns = document.querySelectorAll('.pawn.white');
-
-
 // convert 1D matrix to 2D matrix
 const tmpBoxes = document.getElementsByClassName('box');
 const boxes = [];
@@ -12,16 +8,15 @@ for (let i=0; i<8; i++) {
         boxes[i].push(tmpBoxes[j+i*8]);
     }
 }
-// console.log(boxes[3][5]);
-
-// console.log(whitePawns);
-function whitePawnRules() {
-    
-}
 
 window.onload = function() {
     let chesses = document.querySelectorAll(".chess");
     for (const chess of chesses) {
+        if (chess.classList[1] === 'pawn')
+        {
+            // Fist move of pawn is special
+            chess.firstStep = true;
+        }
         setupDragDrop(chess);
     }
 }
@@ -70,31 +65,115 @@ function setupDragDrop(obj) {
 function move(obj) {
     let superdx = obj.superNewX - obj.superOldX;
     let superdy = obj.superNewY - obj.superOldY;
-    // console.log(obj.offsetLeft,obj.offsetTop);
+    // console.log(superdx,superdy);
     let stepX = Math.round(superdx/80);
     let stepY = Math.round(superdy/80);
-    console.log('MOVEEEEEEEE')
-    console.log(Math.round(superdx/80),Math.round(superdy/80));
-    console.log(superdx,superdy);
-    // obj.offsetParent
+    // console.log('MOVEEEEEEEE')
+    console.log(stepX, stepY);
+    console.log(obj.classList[1]);
+    if (!canIMove(obj, stepX, stepY))
+    {
+        stepX = 0;
+        stepY = 0;
+    }
     for (let i=0; i<8; i++) {
         for (let j=0; j<8; j++) {
             if (boxes[i][j] === obj.offsetParent) {
-                console.log(i,j);
-                console.log(boxes[i][j]);
+                // console.log(i,j);
+                // console.log(boxes[i][j]);
                 if (boxes[i - stepY][j + stepX]) {
-                    console.log(obj);
-                    console.log(boxes[i - stepY][j + stepX]);
+                    // console.log(obj);
+                    // console.log(boxes[i - stepY][j + stepX]);
                     
                     boxes[i - stepY][j + stepX].appendChild(obj);
                     obj.style.left = "50%";
                     obj.style.top = "50%";
                     return;
-                    // boxes[i][j].removeChild(obj);
-                    // console.log(tmpObj);
                 }
             }
         }
     }
     
+}
+
+function canIMove(obj, stepX, stepY) {
+    const func = obj.classList[1];
+    switch (func) {
+        case 'pawn':
+            // care about they faction
+            const faction = obj.classList[2];
+            if (stepX === -1 || stepX === 1) {
+                return canIEat();
+            }
+            if (stepX != 0) {
+                return false;
+            }
+            if (faction === 'white') {
+                if (stepY === -1 || (stepY === -2 && obj.firstStep === true)) {
+                    obj.firstStep = false;
+                        if (!IsStuck()) {
+                        return true;
+                    }
+                }
+            } else if (faction === 'black') {
+                if (stepY === 1 || (stepY === 2 && obj.firstStep === true)) { 
+                    obj.firstStep = false;
+                        if (!IsStuck()) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        
+        case 'rook':
+            if (stepX === 0 || stepY === 0) {
+                if (!IsStuck()) {
+                    return true;
+                }
+            }
+            return false;
+        
+        case 'bishop':
+            if (Math.abs(stepX) === Math.abs(stepY)) {
+                if (!IsStuck()) {
+                    return true;
+                }
+            }
+            return false;
+        
+        case 'queen':
+            if ((Math.abs(stepX) === Math.abs(stepY)) || ((stepX === 0 || stepY === 0))) {
+                if (!IsStuck()) {
+                    return true;
+                }
+            }
+            return false;
+        
+        case 'king':
+            if (Math.abs(stepX) <= 1 && Math.abs(stepY) <= 1) {
+                if (!IsStuck()) {
+                    return true;
+                }
+            }
+            return false;
+        
+        case 'knight':
+            if ((Math.abs(stepX) === 1 && Math.abs(stepY) === 2) || (Math.abs(stepX) === 2 && Math.abs(stepY) === 1)) {
+                if (!IsStuck()) {
+                    return true;
+                }
+            }
+            return false;
+        
+        default:
+            return false;
+    }
+}
+
+function canIEat() {
+    return false;
+}
+
+function IsStuck() {    
+    return false;
 }
