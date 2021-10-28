9130f3323510758fb3c9990609558169ad0721f1
DangPham <dangpham112000@users.noreply.github.com>
2021-10-28 18:26:29 +0700

    fix login bug and some logic register - user sign in app

--- user_sign_in/script.js
+++ user_sign_in/script.js
@@ -7,15 +7,10 @@ $('document').ready(function () {
         $('.login-title').toggleClass('selected');
         $('.register-form').toggleClass('selected');
         $('.login-form').toggleClass('selected');
-
-
     });
-
-    if ($('.login-form').hasClass('selected')) {
-        validationLogin();
-    } else {
-        validationRegistered();
-    }
+    validationLogin();
+    validationRegister();
+    
 });
 
 function validationLogin() {
@@ -31,23 +26,55 @@ function validationLogin() {
 
     $('.login-form').submit(function(event) {
         event.preventDefault();
+        removeAllError();
         
         valiUsername($('#log-username'));
         valiPassword($('#log-password'));
     });
 
-    $('.login').submit((event) => { event.preventDefault(); })
-    $('.login').mousedown(function() {
+    $('#login').submit((event) => { event.preventDefault(); })
+    $('#login').mouseup(function() {
         removeAllError();
     });
 }
 
+function validationRegister() {
+    $('#reg-username').blur(function() { 
+        valiUsername($(this)); 
+    });
+    $('#reg-username').focus(removeElementError);
+
+    $('#reg-password').blur(function() { 
+        valiPassword($(this)); 
+    });
+    $('#reg-password').focus(removeElementError);
+
+    $('#reg-co-password').blur(function() { 
+        valiPassword($(this)); 
+    });
+    $('#reg-co-password').focus(removeElementError);
+
+    $('.register-form').submit(function(event) {
+        event.preventDefault();
+        removeAllError();
+        
+        valiUsername($('#reg-username'));
+        valiPassword($('#reg-password'));
+    });
+
+    $('#register').submit((event) => { event.preventDefault(); })
+    $('#register').mouseup(function() {
+        removeAllError();
+    });
+
+}
+
 function valiUsername(element) {
     const name = element.val();
-    if (!usernameRegex.test(name)) {
-        printError(element, 'Username invalid');
-    } else if (name.length < 6 || name.length > 32) {
+    if (name.length < 6 || name.length > 32) {
         printError(element, 'Username length is betwen from 6 to 32');
+    } else if (!usernameRegex.test(name)) {
+        printError(element, 'Username invalid');
     }
 }
 
@@ -59,9 +86,7 @@ function valiPassword(element) {
 }
 
 function removeElementError() {
-    if ($(this).next('.error')) {
-        $(this).next('.error').remove();
-    }
+    $(this).nextAll('.error').remove();
 }
 
 function removeAllError() {
