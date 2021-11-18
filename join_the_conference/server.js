2da8a493ffe4cd9bfe1519c79bc76da00ec4e1ba
DangPham <dangpham112000@users.noreply.github.com>
2021-11-19 02:00:51 +0700

    join the conference skeleton

---
+++ join_the_conference/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+app.get('/', (req, res) => {
+    res.redirect('index.html');
+});
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
